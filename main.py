a3b38422fabbd40d47e7b5b5b45ad8c7a35f2491
villi02 <villiarnar1@gmail.com>
2024-02-03 18:57:25 +0100

    added test examples

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import numpy as np
 from datetime import datetime, timedelta
 
 
-
 def make_recipt(card_number, items):
     # make recipt
     recipt = {
@@ -17,9 +16,19 @@ def make_recipt(card_number, items):
 
     return recipt
 
+def make_old_recipt(card_number, items):
+    # make recipt
+    recipt = {
+        "student_card_number": card_number,
+        "items": items,
+        "valid_until": datetime.now() + timedelta(minutes=10)
+    }
+
+    return recipt
+
 def print_recipt(recipt):
     # Printing the message and student card number
-    print(recipt['message'])
+    print(recipt['message'] if 'message' in recipt else "")
     print(f"Student Card Number: {recipt['student_card_number']}")
 
     # Printing each item in the purchase
@@ -30,6 +39,11 @@ def print_recipt(recipt):
     # Printing the validity
     valid_until = recipt['valid_until'].strftime('%Y-%m-%d %H:%M:%S')
     print(f"Valid Until: {valid_until}")
+
+
+def check_if_valid(recipt):
+    # Check if the recipt is still valid
+    return recipt['valid_until'] > datetime.now()
 
 # Main function
 def main():
@@ -48,22 +62,47 @@ def main():
             "price": 23.90
         }
     ]
+
+    items_2 = [
+        {
+            "name": "Kaffe",
+            "price": 12.90
+        },
+        {
+            "name": "Red Bull",
+            "price": 33.90
+        }
+    ]
+
+    items_3 = [
+        {
+            "name": "baguette",
+            "price": 12.90
+        },
+        {
+            "name": "Coca Cola Zero",
+            "price": 28.90
+        },
+        {
+            "name": "Litago sjokolademelk",
+            "price": 23.90
+        },
+    ]
+
     recipt = make_recipt(card_number, items_1)
     print_recipt(recipt)
+
+    # check if recipt is valid
+    print(f"is_valid: {check_if_valid(recipt)}")
+
+    print("dine tidligere kjøp:")
+    recipt2 = make_old_recipt(card_number, items_2)
+    print("")
+    recipt3 = make_old_recipt(card_number, items_3)
+    print_recipt(recipt2)
+    print_recipt(recipt3)
     print("done")
 
 # Run the main function
 if __name__ == "__main__":
-    main()
-
-json_template = {
-    "message": "",
-    "can_enter": "",
-    "student": "",
-    "items": [
-        {
-            "name": "",
-            "price": ""
-        }
-    ]
-}
+    main()  
